7df5797def4b823cc08c1a40f0eec5a11e8b5ea2
chikuwa111 <take0shun1111@gmail.com>
2020-06-14 04:20:09 +0900

    feat: Add doGet

--- src/gas/code.ts
+++ src/gas/code.ts
@@ -1,3 +1,5 @@
-function test() {
-  Logger.log("test");
-}
+const doGet = (e: GoogleAppsScript.Events.DoGet) =>
+  HtmlService.createTemplateFromFile("client/page").evaluate();
+
+const includeAsset_ = (filename: string) =>
+  HtmlService.createHtmlOutputFromFile(filename).getContent();
