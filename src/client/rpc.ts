cfc8ee4664dbaae7d3c3c2376ad7888ffcaa2721
chikuwa111 <take0shun1111@gmail.com>
2020-06-14 18:05:09 +0900

    refactor: Not to export google in rpc.ts

--- src/client/rpc.ts
+++ src/client/rpc.ts
@@ -2,13 +2,12 @@ import { RPC } from "../gas/code";
 
 type SuccessHandler = (value: any, obj?: Object) => void;
 type FailureHandler = (error: Error, obj?: Object) => void;
-
 type GoogleScriptRun = RPC & {
   withSuccessHandler: (handler: SuccessHandler) => GoogleScriptRun;
   withFailureHandler: (handler: FailureHandler) => GoogleScriptRun;
 };
 
-export declare const google: {
+declare const google: {
   script: {
     run: GoogleScriptRun;
   };
